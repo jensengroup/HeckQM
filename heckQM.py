6d921b6bde83d0a847ad27ff2928c1b385ccecb2
Nicolai Ree <ree@fend01.cluster>
2022-09-27 22:38:59 +0200

    added

--- heckQM.py
+++ heckQM.py
@@ -389,7 +389,7 @@ if __name__ == "__main__":
     import submitit
 
     # df = pd.read_pickle('data_curation/df_all_data_curated.pkl') #all curated data
-    df = pd.read_pickle('data_cabri/data_cabri.pkl') #Cabri dataset
+    df = pd.read_pickle('data/cabri/data_cabri.pkl') #Cabri dataset
     df = df.tail(1)
 
     executor = submitit.AutoExecutor(folder="submitit_heckqm")
